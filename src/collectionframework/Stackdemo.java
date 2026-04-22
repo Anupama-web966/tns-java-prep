b3aeee31e51c91ad634ed2a4b5af4dbaa2ee3625
Anupama-web966 <vijayvpanupama@gmail.com>
2026-04-22 22:42:25 +0530

    colln

---
+++ src/collectionframework/Stackdemo.java
@@ -0,0 +1,27 @@
+package collectionframework;
+
+public class Stackdemo {
+    Stack<Integer> s=new Stack<Integer>();
+    
+    s.push(2);
+    s.push(4);
+    s.push(4);
+    s.push(8);
+    s.push(9);
+    
+    System.out.println(s);
+    for(int d:s) {
+            System.out.println(d);
+    }
+    // peek()
+    System.out.println(s.peek());
+    System.out.println(s.search(34));
+    
+    s.pop();
+    s.pop();
+    System.out.println(s);
+}
+
+}
+
+
