8229ef8dc4d4084ff687c7abbae555d8254a236e
Anupama-web966 <vijayvpanupama@gmail.com>
2026-04-24 22:27:38 +0530

    stream upi

---
+++ src/com/tnsif/streamapi/SPCDemo.java
@@ -0,0 +1,34 @@
+package com.tnsif.streamapi;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+
+public class SPCDemo {
+    public static void main(String[] args) {
+        
+        // Supplier : no input , return value
+        
+        Supplier<String> s=()->"Hello from supplier";
+        String value=s.get();
+        System.out.println(value);
+        
+        // Predicate : takes one input , return boolean value
+        
+        Predicate<Integer> iseven=number->number%2==0;
+        int testnumber=10;
+        if(iseven.test(testnumber)) {
+                System.out.println(testnumber +"is even");
+        }
+        else {
+                System.out.println(testnumber +"is odd");
+        }
+        // consumer : takes one input , return nothing
+        
+        Consumer<String> c=message->System.out.println("Consumer recieved "+message);
+        c.accept("this is test message");
+        System.out.println(c);
+}
+}
+
+
